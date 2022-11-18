fc24e4c17a4416cac519fe161687d28b7eb8df07
TiePilot <46372133+TIEPilot@users.noreply.github.com>
2022-11-18 11:26:51 +0800

    p111

---
+++ Test01/Checkout/CheckoutState.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CheckoutState.aspx.cs" Inherits="Test01.Checkout.CheckoutState" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+</asp:Content>
--- Test01/Models/ProductContext.cs
+++ Test01/Models/ProductContext.cs
@@ -15,7 +15,8 @@ namespace Test01.Models
         public DbSet<Category>Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CartItem> ShoppingCartItems { get; set; }
-
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
 
 
